1b793a4a1b15c840d78b1a152b74fadfad48261e
pnts <panagiotis.tsivelekidis@gmail.com>
2022-02-04 10:14:46 +0100

    using old names because of hard coded export - fixes css issues

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,16 @@
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addPassthroughCopy({"./src/agb": "agb"})
-  eleventyConfig.addPassthroughCopy({"./src/kontakt": "kontakt"})
-  eleventyConfig.addPassthroughCopy({"./src/datenschutz": "datenschutz"})
-  eleventyConfig.addPassthroughCopy({"./src/felix": "felix"})
+  eleventyConfig.addPassthroughCopy({"./src/agb": "agbs-767"})
+  eleventyConfig.addPassthroughCopy({"./src/kontakt": "kontakt-375"})
+  eleventyConfig.addPassthroughCopy({"./src/datenschutz": "datenschutz-375"})
+  eleventyConfig.addPassthroughCopy({"./src/felix": "teammitglied-felix-375"})
   eleventyConfig.addPassthroughCopy({"./src/home": "/"})
-  eleventyConfig.addPassthroughCopy({"./src/impressum": "impressum"})
-  eleventyConfig.addPassthroughCopy({"./src/netzwerk": "netzwerk"})
-  eleventyConfig.addPassthroughCopy({"./src/pana": "pana"})
-  eleventyConfig.addPassthroughCopy({"./src/referenzen": "referenz"})
-  eleventyConfig.addPassthroughCopy({"./src/service": "service"})
-  eleventyConfig.addPassthroughCopy({"./src/team": "team"})
+  eleventyConfig.addPassthroughCopy({"./src/impressum": "impressum-767"})
+  eleventyConfig.addPassthroughCopy({"./src/netzwerk": "netzwerk-375"})
+  eleventyConfig.addPassthroughCopy({"./src/pana": "teammitglied-pana-375"})
+  eleventyConfig.addPassthroughCopy({"./src/referenzen": "referenzen-767"})
+  eleventyConfig.addPassthroughCopy({"./src/service": "unser-service-375"})
+  eleventyConfig.addPassthroughCopy({"./src/team": "unser-team-375"})
 
   return {
     dir: {
